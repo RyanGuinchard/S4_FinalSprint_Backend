b62930d286460666d471e1e4dc6b9378e3e813b0
Ryan Guinchard <ryanguinchard9@hotmail.com>
2024-07-30 20:28:59 -0230

    Initial Project Created - Branching ready

---
+++ src/test/java/com/keyin/Sprint_Backend/SprintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.Sprint_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
